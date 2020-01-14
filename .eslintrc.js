df0965897f0b85c43264f3bedea0e188cc8ab41d
Bartek Karczmarczyk <bartek.karczmarczyk@gmail.com>
2020-01-14 11:12:07 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "react/state-in-constructor": ["error", 'never'],
+    "react/jsx-fragments": ["error", 'element'],
+    "react/jsx-filename-extension": ["warn", { "extensions": [".js", ".jsx"] }],
+    'no-unused-expressions': ["error", { "allowTernary": true }],
+    "react/destructuring-assignment": ["error", "always", { "ignoreClassFields": true }],
+    "object-curly-newline": ["error", {
+      "ObjectPattern": { "multiline": true, "minProperties": 6 }
+  }]
+  },
+};
